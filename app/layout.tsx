b8c63398d032e627b07900d692f9eaf1029a42eb
Esrafil <esrafil.s.r.man418@gmail.com>
2026-06-23 19:15:56 +0330

    feat: implement Navbar section

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import { ThemeProvider } from "@/components/ThemeProvider";
+import Navbar from "@/components/Navbar";
 
 const geistSans = Geist({
 	variable: "--font-geist-sans",
@@ -26,10 +28,29 @@ export default function RootLayout({
 	return (
 		<ClerkProvider>
 			<html
-				lang="en"
+				lang="en" suppressHydrationWarning
 				className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
 			>
-				<body className="min-h-full flex flex-col">{children}</body>
+				<body className="min-h-full flex flex-col">
+					<ThemeProvider
+						attribute="class"
+						defaultTheme="system"
+						enableSystem
+						disableTransitionOnChange
+					>
+						<div className="min-h-screen">
+							<Navbar />
+							<main className="py-8">
+								<div className="max-w-7xl mx-auto px-4">
+									<div className="grid grid-cols-1 lg:grid-cols-12 gap-6">
+										<div className="hidden lg:block lg:col-span-3">sidebar</div>
+										<div className="lg:col-span-9">{children}</div>
+									</div>
+								</div>
+							</main>
+						</div>
+					</ThemeProvider>
+				</body>
 			</html>
 		</ClerkProvider>
 	);
--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,3 @@
-import { Show, SignInButton, SignUpButton, UserButton } from "@clerk/nextjs";
-
 export default function Home() {
-	return (
-		<header className="flex justify-end items-center p-4 gap-4 h-16">
-			<Show when="signed-out">
-				<SignInButton mode="modal" />
-				<SignUpButton mode="modal">
-					<button
-						type="button"
-						className="bg-[#6c47ff] text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer"
-					>
-						Sign Up
-					</button>
-				</SignUpButton>
-			</Show>
-			<Show when="signed-in">
-				<UserButton />
-			</Show>
-		</header>
-	);
+	return <h1>Home</h1>;
 }
